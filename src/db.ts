b0d3cecc1de3050b0c12974108467ecde5f049a5
shadid12 <shadidhaque2012@gmail.com>
2022-12-28 10:14:11 -0500

    update code

--- src/db.ts
+++ src/db.ts
@@ -4,6 +4,7 @@ export const q = faunadb.query as any;
 declare const FAUNA_DOMAIN: string;
 declare const FAUNA_SECRET: string;
 
+
 export const faunaClient = new faunadb.Client({ 
   domain: FAUNA_DOMAIN,
   secret: FAUNA_SECRET,
